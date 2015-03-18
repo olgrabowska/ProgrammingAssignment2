3dac5281868e82703ae9d30bbd2ea173922c95ea
Aleksandra <ol.grabowska@gmail.com>
2015-03-18 13:22:31 +0100

    Change of cachematrix.R file so that it contains neccessary functions.

--- cachematrix.R
+++ cachematrix.R
@@ -23,7 +23,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## The following function calculates the inverse of the special "matrix" created with the above function. However, it first checks to see if the inverse has already been calculated. If so, it gets the inverse from the cache and skips the computation. Otherwise, it calculates the inverse of the data and sets the value of the inverse in the cache via the setinverse function.
+## The following function calculates the inverse of the special "matrix" created with the above
+# function. However, it first checks to see if the inverse has already been calculated. If so, it 
+# gets the inverse from the cache and skips the computation. Otherwise, it calculates the inverse 
+# the data and sets the value of the inverse in the cache via the setinverse function.
 
 'Warning! The matrix has to be invertible, otherwise the function won''t work.'
 
